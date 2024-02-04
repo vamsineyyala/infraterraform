7e648e36d28e55d503b5926263b162592825b6ed
sasikanth53 <42739097+sasikanth53@users.noreply.github.com>
2024-02-04 13:36:39 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hashtek-terraform-state-bucket-123"
+    bucket = "terraform-state-bucket-0402"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "hashtekTerraformStatelock"
