2552d47bffe9dd942e1cbb3f849dcf87be7634cf
vamsineyyala <127283897+vamsineyyala@users.noreply.github.com>
2024-02-29 17:31:36 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket-2702"
+    bucket = "terraform-state-bucket-2901"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "hashtekTerraformStatelock"
