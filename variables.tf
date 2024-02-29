241efd5a61a700ac66b88f0a99c258692db39d1c
vamsineyyala <127283897+vamsineyyala@users.noreply.github.com>
2024-02-29 17:55:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "admin-sasi-key" 
+    default     =  "vamsikey" 
 }
 
 variable "instance_type" { 
