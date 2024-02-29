349cbf1d5d5626aae40bd2865b3a2d77dd2ecbbf
vamsineyyala <127283897+vamsineyyala@users.noreply.github.com>
2024-02-29 18:58:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -39,7 +39,7 @@ variable "acl" {
 variable "bucket_prefix" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "my-s3bucket-"
+    default     = "vamsi-s3bucket-2024"# the name of the bucket must be unique here bucket prefix is nothing but the bucket name
 }
 variable "tags" {
     type        = map
